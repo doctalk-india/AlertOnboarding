2337b7242e5569b2d9ce980edd9fdd4770d8a1a6
Sourav Chandra <saurav.chandra1992@live.com>
2019-03-28 14:35:30 +0530

    Carthage only framework

---
+++ AlertOnboarding/AlertOnboardingFramework.h
@@ -0,0 +1,19 @@
+//
+//  AlertOnboardingFramework.h
+//  AlertOnboardingFramework
+//
+//  Created by Sourav Chandra on 28/03/19.
+//  Copyright © 2019 CookMinute. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AlertOnboardingFramework.
+FOUNDATION_EXPORT double AlertOnboardingFrameworkVersionNumber;
+
+//! Project version string for AlertOnboardingFramework.
+FOUNDATION_EXPORT const unsigned char AlertOnboardingFrameworkVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AlertOnboardingFramework/PublicHeader.h>
+
+
